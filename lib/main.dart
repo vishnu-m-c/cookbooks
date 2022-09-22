3ae6ac35eb9c67a66fcca469b8ee941a732be4a6
vishnu.mc <vishnu.mc@travancoreanalytics.com>
2022-09-22 15:29:45 +0530

    image inserted at drawer header

--- lib/main.dart
+++ lib/main.dart
@@ -113,6 +113,7 @@ class Home extends StatelessWidget {
           children: [
             const DrawerHeader(
               decoration: BoxDecoration(
+                image: DecorationImage(image: NetworkImage('https://images.unsplash.com/photo-1517960413843-0aee8e2b3285?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZmVlbCUyMGdvb2R8ZW58MHx8MHx8&w=1000&q=80')),
                 color: Colors.blue,
               ),
               child: Text('Drawer Header'),
